660aed594adead6a6a0503e0b345292d2d239e8b
Peeber <nargonnarwhalz@gmail.com>
2024-03-25 09:50:55 -0400

    created knockback component and integrated it with hitbox, added one to spark, and set up statuses array in Attack.gd

---
+++ components/KnockbackComponent.gd
@@ -0,0 +1,12 @@
+extends Node2D
+class_name KnockbackComponent
+
+@onready var parent = get_parent()
+
+func knockback(source,force,direction : Vector2):
+	if not direction:
+		var source_position = get_node(source).position
+		var assumed_direction = source_position - parent.position
+		Physics.throw(parent,assumed_direction,force)
+	else:
+		Physics.throw(parent,direction,force)
